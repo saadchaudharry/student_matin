a3ded3618fa66baa24de936fa7a54462feaea2d0
saadindictrans <saad.c@indictranstech.com>
2023-04-01 14:45:36 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Student App
+
+s
+
+#### License
+
+s
---
+++ student_app/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Student App",
+			"color": "s",
+			"icon": "s",
+			"type": "module",
+			"label": _("Student App")
+		}
+	]
